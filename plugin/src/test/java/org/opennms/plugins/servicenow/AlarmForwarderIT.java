04a708436bb7a97a7eef969e91ac8a75be640830
Antonio Russo <rssntn67@yahoo.it>
2025-07-13 22:35:56 +0200

    Fixed IT tests

--- plugin/src/test/java/org/opennms/plugins/servicenow/AlarmForwarderIT.java
+++ plugin/src/test/java/org/opennms/plugins/servicenow/AlarmForwarderIT.java
@@ -9,8 +9,12 @@ import org.opennms.integration.api.v1.model.Severity;
 import org.opennms.integration.api.v1.model.immutables.ImmutableAlarm;
 import org.opennms.plugins.servicenow.client.ApiClientCredentials;
 import org.opennms.plugins.servicenow.client.ApiClientProvider;
+import org.opennms.plugins.servicenow.client.ApiClientProviderImpl;
+import org.opennms.plugins.servicenow.client.ClientManager;
+import org.opennms.plugins.servicenow.connection.Connection;
 import org.opennms.plugins.servicenow.connection.ConnectionManager;
 
+import java.util.Optional;
 import java.util.concurrent.TimeUnit;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
@@ -22,6 +26,7 @@ import static com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo;
 import static com.github.tomakehurst.wiremock.client.WireMock.verify;
 import static org.awaitility.Awaitility.await;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class AlarmForwarderIT {
 
@@ -31,19 +36,15 @@ public class AlarmForwarderIT {
     @Test
     public void canForwardAlarm() {
         // Wire it up
-        ApiClientCredentials apiClientCredentials = ApiClientCredentials.builder()
-                .withUrl(wireMockRule.url("/"))
-                .withUsername("username")
-                .withPassword("password")
-                .withIgnoreSslCertificateValidation(true)
-                .build();
         EventForwarder eventForwarder = mock(EventForwarder.class);
         ConnectionManager connectionManager = mock(ConnectionManager.class);
-        ApiClientProvider apiClientProvider = mock(ApiClientProvider.class);
+        ApiClientProvider apiClientProvider = new ApiClientProviderImpl();
         AlarmForwarder alarmForwarder = new AlarmForwarder(connectionManager,apiClientProvider, eventForwarder, "CategoryA");
 
+        when(connectionManager.getConnection()).thenReturn(Optional.of(new ConnectionTest()));
+
         // Stub the endpoint
-        stubFor(post((urlEqualTo("/crea_aggiorna_allarmi")))
+        stubFor(post((urlEqualTo("//crea_aggiorna_allarmi")))
                 .willReturn(aResponse()
                         .withStatus(200)));
 
@@ -54,8 +55,61 @@ public class AlarmForwarderIT {
         await().atMost(15, TimeUnit.SECONDS)
                 .catchUncaughtExceptions()
                 .until(() -> {
-                    verify(1, postRequestedFor(urlPathEqualTo("/crea_aggiorna_allarmi")));
+                    verify(1, postRequestedFor(urlPathEqualTo("//crea_aggiorna_allarmi")));
                     return true;
                 });
+    }
+
+    private class ConnectionTest implements Connection {
+
+        @Override
+        public boolean isIgnoreSslCertificateValidation() {
+            return true;
+        }
+
+        @Override
+        public void setIgnoreSslCertificateValidation(boolean ignoreSslCertificateValidation) {
+
+        }
+
+        @Override
+        public String getUrl() {
+            return wireMockRule.url("/");
+        }
+
+        @Override
+        public void setUrl(String url) {
+
+        }
+
+        @Override
+        public String getUsername() {
+            return "username";
+        }
+
+        @Override
+        public void setUsername(String username) {
+
+        }
+
+        @Override
+        public String getPassword() {
+            return "password";
+        }
+
+        @Override
+        public void setPassword(String password) {
+
+        }
+
+        @Override
+        public void save() {
+
+        }
+
+        @Override
+        public void delete() {
+
+        }
     }
 }
