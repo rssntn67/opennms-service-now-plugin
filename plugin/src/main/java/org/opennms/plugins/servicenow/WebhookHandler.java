612a979bce7d779f27314bbd7c787d41cb32c18a
Antonio Russo <rssntn67@yahoo.it>
2025-07-20 07:57:55 +0200

    Cleaned imports

--- plugin/src/main/java/org/opennms/plugins/servicenow/WebhookHandler.java
+++ plugin/src/main/java/org/opennms/plugins/servicenow/WebhookHandler.java
@@ -1,11 +1,7 @@
 package org.opennms.plugins.servicenow;
 
-import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @Path("opennms-service-now")
