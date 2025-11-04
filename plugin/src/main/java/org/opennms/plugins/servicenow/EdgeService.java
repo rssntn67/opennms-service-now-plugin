3a1225505a5c9eb875d75278f0c86ec9b0ff3994
Antonio Russo <rssntn67@yahoo.it>
2025-11-04 18:05:40 +0100

    Using different strategy for get node parent

--- plugin/src/main/java/org/opennms/plugins/servicenow/EdgeService.java
+++ plugin/src/main/java/org/opennms/plugins/servicenow/EdgeService.java
@@ -9,55 +9,55 @@ import org.opennms.integration.api.v1.health.Status;
 import org.opennms.integration.api.v1.health.immutables.ImmutableResponse;
 import org.opennms.integration.api.v1.model.MetaData;
 import org.opennms.integration.api.v1.model.Node;
+import org.opennms.integration.api.v1.model.NodeCriteria;
 import org.opennms.integration.api.v1.model.TopologyEdge;
 import org.opennms.integration.api.v1.model.TopologyPort;
 import org.opennms.integration.api.v1.model.TopologyProtocol;
 import org.opennms.integration.api.v1.model.TopologySegment;
-import org.opennms.integration.api.v1.topology.TopologyEdgeConsumer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
-import java.util.Set;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 
-public class EdgeService implements Runnable, HealthCheck , TopologyEdgeConsumer {
+public class EdgeService implements Runnable, HealthCheck {
 
-    private static class EdgeServiceVisitor implements TopologyEdge.EndpointVisitor {
+    private class EdgeServiceVisitor implements TopologyEdge.EndpointVisitor {
         String parent;
         String child;
-        final String filter;
-
-        private EdgeServiceVisitor(String filter) {
-            this.filter = filter;
-        }
 
         @Override
         public void visitSource(Node node) {
             LOG.info("EdgeServiceVisitor:visitSource:Node {}", node);
-            if (node.getCategories().contains(filter))
-                child = node.getLabel();
-        }
-
-        @Override
-        public void visitTarget(Node node) {
-            LOG.info("EdgeServiceVisitor:visitTarget:Node {}", node);
             parent = node.getLabel();
         }
 
         @Override
         public void visitSource(TopologyPort port) {
             LOG.info("EdgeServiceVisitor:visitSource:TopologyPort {}", port);
+            NodeCriteria nodeCriteria = port.getNodeCriteria();
+            Node node = nodeDao.getNodeByForeignSourceAndForeignId(nodeCriteria.getForeignSource(),nodeCriteria.getForeignId());
+            parent = node.getLabel();
         }
+
+        @Override
+        public void visitTarget(Node node) {
+            LOG.info("EdgeServiceVisitor:visitTarget:Node {}", node);
+            child = node.getLabel();
+        }
+
 
         @Override
         public void visitTarget(TopologyPort port) {
             LOG.info("EdgeServiceVisitor:visitTarget:TopologyPort {}", port);
+            NodeCriteria nodeCriteria = port.getNodeCriteria();
+            Node node = nodeDao.getNodeByForeignSourceAndForeignId(nodeCriteria.getForeignSource(),nodeCriteria.getForeignId());
+            child = node.getLabel();
         }
 
         @Override
@@ -65,6 +65,10 @@ public class EdgeService implements Runnable, HealthCheck , TopologyEdgeConsumer
             LOG.info("EdgeServiceVisitor:visitTarget:TopologySegment {}", segment);
         }
 
+        public void clean() {
+            parent=null;
+            child=null;
+        }
         public String getParent() {
             return parent;
         }
@@ -80,27 +84,21 @@ public class EdgeService implements Runnable, HealthCheck , TopologyEdgeConsumer
     private final EdgeDao edgeDao;
     private final String context;
     private final String parentKey;
-    private final String gatewayKey;
-    private final String filter;
 
     private final ScheduledFuture<?> scheduledFuture;
 
-    Map<String, String> parentMap = new HashMap<>();
+    private final Map<String, String> parentMap = new HashMap<>();
 
     public EdgeService(EdgeDao edgeDao,
                        NodeDao nodeDao,
                        String initialDelay,
                        String delay,
-                       String filter,
                        String context,
-                       String parentKey,
-                       String gatewayKey) {
+                       String parentKey) {
         this.edgeDao = Objects.requireNonNull(edgeDao);
         this.nodeDao = Objects.requireNonNull(nodeDao);
         this.context = Objects.requireNonNull(context);
         this.parentKey = Objects.requireNonNull(parentKey);
-        this.gatewayKey = Objects.requireNonNull(gatewayKey);
-        this.filter = Objects.requireNonNull(filter);
 
         ScheduledExecutorService scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
         scheduledFuture = scheduledExecutorService.scheduleWithFixedDelay(this, Long.parseLong(initialDelay),
@@ -118,24 +116,25 @@ public class EdgeService implements Runnable, HealthCheck , TopologyEdgeConsumer
                 return m.getValue();
             }
         }
-        for (MetaData m : node.getMetaData()) {
-            if (m.getContext().equals(context) && m.getKey().equals(gatewayKey)) {
-                return parentMap.get(node.getLabel());
-            }
-        }
-
+        if (parentMap.containsKey(node.getLabel()))
+            return parentMap.get(node.getLabel());
         return "NoParentNodeFound";
     }
 
     @Override
     public void run() {
-        Set<TopologyEdge> edges = edgeDao.getEdges();
-        TopologyEdge edge = edges.iterator().next();
-        EdgeServiceVisitor visitor = new EdgeServiceVisitor(filter);
-        edge.visitEndpoints(visitor);
-        if (visitor.getChild() != null) {
-            parentMap.put(visitor.child, visitor.parent);
-        }
+        parentMap.clear();
+        final EdgeServiceVisitor visitor = new EdgeServiceVisitor();
+        edgeDao.getEdges()
+                .stream()
+                .filter(e -> e.getProtocol() == TopologyProtocol.LLDP)
+                .forEach( edge -> {
+                    edge.visitEndpoints(visitor);
+                    if (visitor.getChild() != null) {
+                        parentMap.put(visitor.getChild(), visitor.getParent());
+                    }
+                    visitor.clean();
+                });
     }
 
     @Override
@@ -144,25 +143,11 @@ public class EdgeService implements Runnable, HealthCheck , TopologyEdgeConsumer
     }
 
     @Override
-    public Response perform(Context context) throws Exception {
+    public Response perform(Context context) {
         return ImmutableResponse.newBuilder()
                 .setStatus(scheduledFuture.isDone() ? Status.Failure : Status.Success)
                 .setMessage(scheduledFuture.isDone() ? "Not running" : "Running")
                 .build();
-    }
-
-    @Override
-    public void onEdgeAddedOrUpdated(TopologyEdge topologyEdge) {
-    }
-
-    @Override
-    public void onEdgeDeleted(TopologyEdge topologyEdge) {
-
-    }
-
-    @Override
-    public Set<TopologyProtocol> getProtocols() {
-        return Set.of(edgeDao.getProtocols().toArray(new TopologyProtocol[0]));
     }
 
     public void destroy() {
