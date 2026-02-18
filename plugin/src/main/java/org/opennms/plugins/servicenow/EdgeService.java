add4d9e0daaf89f66bc0b03aeef05e8138bc3618
Antonio Russo <rssntn67@yahoo.it>
2026-02-18 08:51:01 +0100

    Move EdgeService initialization from constructor back to init()

--- plugin/src/main/java/org/opennms/plugins/servicenow/EdgeService.java
+++ plugin/src/main/java/org/opennms/plugins/servicenow/EdgeService.java
@@ -145,6 +145,21 @@ public class EdgeService implements Runnable, HealthCheck {
     private final Integer maxIteration;
 
     public void init() {
+        parentByGatewayKeyMap = new ConcurrentHashMap<>();
+        LOG.info("init: parentMap initialized: {}", this.parentByGatewayKeyMap != null);
+        LOG.info("init: parentMap size: {}", this.parentByGatewayKeyMap.size());
+        LOG.info("init: parentMap class: {}", this.parentByGatewayKeyMap.getClass().getName());
+        LOG.info("init: this reference: {}", this);
+        ScheduledExecutorService scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
+        scheduledFuture =
+                scheduledExecutorService
+                        .scheduleWithFixedDelay(
+                                this,
+                                initialDelayL,
+                                delayL,
+                                TimeUnit.MILLISECONDS
+                        );
+        LOG.info("init: Scheduler initialized, parentMap: {}", this.parentByGatewayKeyMap.size());
     }
 
 
@@ -169,21 +184,6 @@ public class EdgeService implements Runnable, HealthCheck {
         this.excludedForeignSource = Objects.requireNonNull(excludedForeignSource);
         this.initialDelayL =  Long.parseLong(Objects.requireNonNull(initialDelay));
         this.delayL =  Long.parseLong(Objects.requireNonNull(delay));
-        parentByGatewayKeyMap = new ConcurrentHashMap<>();
-        LOG.info("init: parentMap initialized: {}", this.parentByGatewayKeyMap != null);
-        LOG.info("init: parentMap size: {}", this.parentByGatewayKeyMap.size());
-        LOG.info("init: parentMap class: {}", this.parentByGatewayKeyMap.getClass().getName());
-        LOG.info("init: this reference: {}", this);
-        ScheduledExecutorService scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
-        scheduledFuture =
-                scheduledExecutorService
-                        .scheduleWithFixedDelay(
-                                this,
-                                initialDelayL,
-                                delayL,
-                                TimeUnit.MILLISECONDS
-                        );
-        LOG.info("init: Scheduler initialized, parentMap: {}", this.parentByGatewayKeyMap.size());
     }
     public Set<String> getLocations() {
         return new HashSet<>(this.locations);
