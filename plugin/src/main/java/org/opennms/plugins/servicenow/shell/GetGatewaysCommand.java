2f0e476f95e7175dd8a1ff9490c05b94dcea2e53
Antonio Russo <rssntn67@yahoo.it>
2026-02-21 16:44:26 +0100

    Restore GetGatewaysCommand

---
+++ plugin/src/main/java/org/opennms/plugins/servicenow/shell/GetGatewaysCommand.java
@@ -0,0 +1,34 @@
+package org.opennms.plugins.servicenow.shell;
+
+import org.apache.karaf.shell.api.action.Action;
+import org.apache.karaf.shell.api.action.Command;
+import org.apache.karaf.shell.api.action.lifecycle.Reference;
+import org.apache.karaf.shell.api.action.lifecycle.Service;
+import org.apache.karaf.shell.api.console.Session;
+import org.apache.karaf.shell.support.table.Col;
+import org.apache.karaf.shell.support.table.ShellTable;
+import org.opennms.plugins.servicenow.EdgeService;
+
+@Command(scope = "opennms-service-now", name = "get-gateways", description = "Get List of Gateways.")
+@Service
+public class GetGatewaysCommand implements Action {
+
+    @Reference
+    private Session session;
+
+    @Reference
+    private EdgeService service;
+
+    @Override
+    public Object execute() {
+        final var table = new ShellTable()
+                .size(session.getTerminal().getWidth() - 1)
+                .column(new Col("gateway").maxSize(72));
+        for (String gateway: service.getGateways()) {
+            final var row = table.addRow();
+            row.addContent(gateway);
+        }
+        table.print(System.out,true);
+        return null;
+    }
+}
